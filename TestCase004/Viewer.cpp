97912e7b0bfe93d26a2279130982daf32770638a
xinwuya <xinwuya_scu@163.com>
2019-04-27 01:48:43 +0800

    添加测试用例004，整合Camera和Texture功能

---
+++ TestCase004/Viewer.cpp
@@ -0,0 +1,145 @@
+#include "Viewer.h"
+
+CViewer::CViewer(const std::string& WindowTitle, int vWindowWidth, int vWindowHeight) : CGLScreen(WindowTitle, vWindowWidth, vWindowHeight), m_WindowWidth(vWindowWidth), m_WindowHeight(vWindowHeight)
+{
+	m_LastMouseX = static_cast<float>(vWindowWidth) / 2.0;
+	m_LastMouseY = static_cast<float>(vWindowHeight) / 2.0;
+
+	m_pCamera = std::make_shared<CCamera>();
+	m_pTexture = std::make_shared<CGLTexture>("textures/container2.png");
+	m_pGLShader = std::make_shared<CGLShader>();
+	m_pGLShader->initFromFiles("triangle", "shaders/triangle.vert", "shaders/triangle.frag");
+	
+	std::vector<glm::vec3> Vertices = {
+		glm::vec3(-0.5f, -0.5f, 0.0f),
+		glm::vec3( 0.5f, -0.5f, 0.0f),
+		glm::vec3(-0.5f,  0.5f, 0.0f),
+		glm::vec3( 0.5f,  0.5f, 0.0f)
+	};
+
+	std::vector<glm::ivec3> Indices =
+	{
+		glm::ivec3(0, 1, 2),
+		glm::ivec3(1, 2, 3)
+	};
+
+	std::vector<glm::vec2> TextureCoord = {
+		glm::vec2(0.0f, 0.0f),
+		glm::vec2(1.0f, 0.0f),
+		glm::vec2(0.0f, 1.0f),
+		glm::vec2(1.0f, 1.0f)
+	};
+
+	m_pGLShader->bind();
+	m_pGLShader->uploadAttrib("Pos", Vertices, 3);
+	m_pGLShader->uploadAttrib("Indices", Indices, 3);
+	m_pGLShader->uploadAttrib("TextureCoord", TextureCoord, 2);
+
+	m_pTexture->bind(m_pTexture->getTextureID());
+	m_pGLShader->setIntUniform("u_Texture", m_pTexture->getTextureID());
+}
+
+CViewer::~CViewer()
+{
+}
+
+//***********************************************************************************************
+//Function:
+void CViewer::drawContents()
+{
+	double CurrentTime = glfwGetTime();
+	m_DeltaTime = CurrentTime - m_LastGLFWTime;
+	m_LastGLFWTime = CurrentTime;
+	__processInput();
+
+	glm::mat4 Projection = m_pCamera->computeProjectionMatrix(static_cast<float>(m_WindowWidth) / static_cast<float>(m_WindowHeight));
+	glm::mat4 View = m_pCamera->getViewMatrix();
+	glm::mat4 Model;
+	m_pGLShader->bind();
+	m_pGLShader->setMat4Uniform("projection", &Projection[0][0]);
+	m_pGLShader->setMat4Uniform("view", &View[0][0]);
+	m_pGLShader->setMat4Uniform("model", &Model[0][0]);
+	
+	//m_pGLShader->drawArray(GL_TRIANGLES, 0, 6);
+	m_pGLShader->drawIndexed(GL_TRIANGLES, 0, 2);
+}
+
+//***********************************************************************************************
+//Function:
+void CViewer::processCursorPosCallbackEvent(double vX, double vY)
+{
+	if (m_IsFirstMouse)
+	{
+		m_LastMouseX = vX;
+		m_LastMouseY = vY;
+		m_IsFirstMouse = false;
+	}
+
+	float XOffset = vX - m_LastMouseX;
+	float YOffset = m_LastMouseY - vY;
+
+	m_LastMouseX = vX;
+	m_LastMouseY = vY;
+	m_pCamera->processCursorMovementEvent(XOffset, YOffset);
+}
+
+//***********************************************************************************************
+//Function:
+void CViewer::processMouseButtonCallbackEvent(int vButton, int vAction, int vModifiers)
+{
+	if (vAction == GLFW_PRESS)
+	{
+		if (vButton == GLFW_MOUSE_BUTTON_LEFT)
+			m_pCamera->setMoveState(true);
+	}
+	else
+		m_pCamera->setMoveState(false);
+}
+
+//***********************************************************************************************
+//Function:
+void CViewer::processKeyCallbackEvent(int vKey, int vScancode, int vAction, int vMods)
+{
+	
+
+	//if (vAction == GLFW_PRESS)
+	{
+		/*switch (vKey)
+		{
+		case GLFW_KEY_ESCAPE: glfwSetWindowShouldClose(m_pGLFWWindow, true); break;
+		case GLFW_KEY_W: m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_FORWARD, DeltaTime); break;
+		case GLFW_KEY_S: m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_BACKWARD, DeltaTime); break;
+		case GLFW_KEY_A: m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_LEFT, DeltaTime); break;
+		case GLFW_KEY_D: m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_RIGHT, DeltaTime); break;
+		case GLFW_KEY_E: m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_UP, DeltaTime); break;
+		case GLFW_KEY_Q: m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_DOWN, DeltaTime); break;
+		default: break;
+		}*/
+		if (vKey == GLFW_KEY_ESCAPE && vAction == GLFW_PRESS) glfwSetWindowShouldClose(fetchGLFWWindow(), true);
+		/*if (vKey == GLFW_KEY_W) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_FORWARD, m_DeltaTime);
+		if (vKey == GLFW_KEY_S) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_BACKWARD, m_DeltaTime);
+		if (vKey == GLFW_KEY_A) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_LEFT, m_DeltaTime);
+		if (vKey == GLFW_KEY_D) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_RIGHT, m_DeltaTime);
+		if (vKey == GLFW_KEY_Q) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_UP, m_DeltaTime);
+		if (vKey == GLFW_KEY_E) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_DOWN, m_DeltaTime);*/
+	}
+}
+
+//***********************************************************************************************
+//Function:
+void CViewer::processScrollCallbackEvent(double vX, double vY)
+{
+	m_pCamera->processMouseScrollEvent(vY);
+}
+
+//***********************************************************************************************
+//Function:
+void CViewer::__processInput()
+{
+	if (glfwGetKey(fetchGLFWWindow(), GLFW_KEY_W) == GLFW_PRESS) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_FORWARD, m_DeltaTime);
+	if (glfwGetKey(fetchGLFWWindow(), GLFW_KEY_S) == GLFW_PRESS) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_BACKWARD, m_DeltaTime);
+	if (glfwGetKey(fetchGLFWWindow(), GLFW_KEY_A) == GLFW_PRESS) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_LEFT, m_DeltaTime);
+	if (glfwGetKey(fetchGLFWWindow(), GLFW_KEY_D) == GLFW_PRESS) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_RIGHT, m_DeltaTime);
+	if (glfwGetKey(fetchGLFWWindow(), GLFW_KEY_Q) == GLFW_PRESS) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_UP, m_DeltaTime);
+	if (glfwGetKey(fetchGLFWWindow(), GLFW_KEY_E) == GLFW_PRESS) m_pCamera->processKeyEvent(gl_kernel::ECameraMovement::CAMERA_MOVE_DOWN, m_DeltaTime);
+}
