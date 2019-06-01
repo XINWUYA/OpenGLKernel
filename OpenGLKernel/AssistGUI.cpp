d268e31822de60e59e2da0e370c11231bb215be1
xinwuya <xinwuya_scu@163.com>
2019-06-02 00:53:15 +0800

    更新常用的GUI功能，去掉GUI类

--- OpenGLKernel/AssistGUI.cpp
+++ OpenGLKernel/AssistGUI.cpp
@@ -3,6 +3,8 @@
 #include "examples/imgui_impl_glfw.h"
 #include "examples/imgui_impl_opengl3.h"
 #include "GLScreen.h"
+#include <iostream>
+#include <cstdarg>
 
 NAMESPACE_BEGIN(gl_kernel)
 
@@ -39,12 +41,14 @@ void CAssistGUI::updateGUI()
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplGlfw_NewFrame();
 	ImGui::NewFrame();
+	
+	ImGui::Begin(m_Title.c_str());
 
 	if (m_IsShowDefaultGUI)
 	{
-		ImGui::Begin(m_Title.c_str());
+		static float f = 0.0f;
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		ImGui::End();
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
 	}
 }
 
@@ -52,8 +56,34 @@ void CAssistGUI::updateGUI()
 //Function:
 void CAssistGUI::drawGUI()
 {
+	ImGui::End();
+
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+//***********************************************************************************************
+//Function:
+void CAssistGUI::text(const char* vStr, ...)
+{
+	va_list args;
+	va_start(args, vStr);
+	ImGui::TextV(vStr, args);
+	va_end(args);
+}
+
+//***********************************************************************************************
+//Function:
+bool CAssistGUI::sliderFloat(const std::string& vName, float& vioValue, float vMin, float vMax)
+{
+	return ImGui::SliderFloat(vName.c_str(), &vioValue, vMin, vMax);
+}
+
+//***********************************************************************************************
+//Function:
+bool CAssistGUI::colorEdit3(const std::string& vName, float* vColor)
+{
+	return ImGui::ColorEdit3(vName.c_str(), vColor);
 }
 
 NAMESPACE_END(gl_kernel)
--- OpenGLKernel/AssistGUI.h
+++ OpenGLKernel/AssistGUI.h
@@ -17,12 +17,16 @@ public:
 	void updateGUI();
 	void drawGUI();
 
+	void text(const char* vStr, ...);
+	bool sliderFloat(const std::string& vName, float& vioValue, float vMin, float vMax);
+	bool colorEdit3(const std::string& vName, float* vColor);
+
 	void setIsShowDefaultGUI(bool vIsShowDefaultGUI) { m_IsShowDefaultGUI = vIsShowDefaultGUI; }
 
 private:
 	ImGuiIO* m_pImGuiIO = nullptr;
 	std::string m_Title;
-	bool m_IsShowDefaultGUI = true;
+	bool m_IsShowDefaultGUI = false;
 };
 
 NAMESPACE_END(gl_kernel)
