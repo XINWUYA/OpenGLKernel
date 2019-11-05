8e5b8d9fac8c4ac27507d4f41b4575520bd9c44c
XINWUYA <xinwuya_scu@163.com>
2019-11-05 13:16:45 +0800

    修正点光源阴影效果

--- ShaderSources/LN021/CubeFragShader.glsl
+++ ShaderSources/LN021/CubeFragShader.glsl
@@ -10,7 +10,7 @@ uniform sampler2D uTexture;
 uniform samplerCube u_ShadowMapTex;
 uniform float u_FarPlane;
 uniform vec3 uCameraPos;
-uniform vec3 u_LightPos = vec3(-2.0f, 4.0f, -1.0f);
+uniform vec3 u_LightPos;
 uniform bool u_IsScence = false;
 
 vec3 LightColor = vec3(1.0);
@@ -21,15 +21,11 @@ float ShadowCalculation(vec3 fragPos)
 	vec3 fragToLight = fragPos - u_LightPos;
 	// Use the light to fragment vector to sample from the depth map    
 	float closestDepth = texture(u_ShadowMapTex, normalize(fragToLight)).r;
-	// It is currently in linear range between [0,1]. Re-transform back to original value
-	//closestDepth *= u_FarPlane;
-	// Now get current linear depth as the length between the fragment and light position
-	float currentDepth = length(fragToLight);
 	// Now test for shadows
-	float bias = 0.05;
-	float shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
+	float bias = 0.05f;
+	float shadow = length(fragToLight) - bias > closestDepth * u_FarPlane ? 1.0 : 0.0;
 
-	return closestDepth;
+	return shadow;
 }
 
 void main()
@@ -56,7 +52,8 @@ void main()
 	float Shadow = ShadowCalculation(v2f_FragPos);
 	vec3 ResultColor = (Ambient + (1.0 - Shadow) * (Diffuse + Specular)) * AlbedoColor;
 
-	//FragColor = vec4(ResultColor, 1.0);
-	FragColor = vec4(vec3(texture(u_ShadowMapTex, normalize(v2f_FragPos)).g), 1.0);
-	//FragColor = vec4(normalize(vec3(v2f_FragPos)), 1.0);
+	FragColor = vec4(ResultColor, 1.0);
+	//FragColor = vec4(vec3(texture(u_ShadowMapTex, normalize(v2f_FragPos))), 1.0);
+	//FragColor = vec4(vec3(texture(u_ShadowMapTex, normalize(vec3(1))).r), 1.0);
+	//FragColor = vec4(vec3(Shadow), 1.0);
 }
